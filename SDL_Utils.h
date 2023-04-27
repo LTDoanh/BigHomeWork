ea4bd92997610bdda96133921b8b0f1354b5daef
LTDoanh <125759416+LTDoanh@users.noreply.github.com>
2023-04-27 17:09:56 +0700

    Version 2.0
    Delete wrong file

--- SDL_Utils.h
+++ SDL_Utils.h
@@ -1,17 +1,1 @@
-#pragma once
 
-#include "Project_Base.h"
-#include "LTexture.h"
-#include "Ship.h"
-
-static Ship ship;
-static int score = 0;
-static bool quit = false;
-
-void update();
-
-void handleEvents();
-
-void render();
-
-// SDL_UTILS_H_INCLUDED
